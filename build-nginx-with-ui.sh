76e47eb4b8cd68517124eb9277fd3586ed91dc4d
tuon <976056042@qq.com>
2023-11-30 14:42:53 +0800

    1、minor: 更换为阿里云镜像

--- build-nginx-with-ui.sh
+++ build-nginx-with-ui.sh
@@ -1,3 +1,3 @@
 #/usr/bin/sh
 export DOCKER_BUILDKIT=1
-docker build . -f docker/Dockerfile-with-nginx  -t tuonina/nginx-with-ui:latest
+docker build . -f docker/Dockerfile-with-nginx  -t registry.cn-hangzhou.aliyuncs.com/tuon-pub/nginx-with-ui:latest
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #/usr/bin/sh
 export DOCKER_BUILDKIT=1
-docker build . -f docker/Dockerfile  -t tuonina/nginx-ui:latest
+docker build . -f docker/Dockerfile  -t registry.cn-hangzhou.aliyuncs.com/tuon-pub/nginx-ui:latest
