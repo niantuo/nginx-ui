d1e96c0d43b75f9eacf8990b4039641fdba31733
tuon <976056042@qq.com>
2023-12-13 14:33:42 +0800

    da

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #/usr/bin/sh
+dos2unix docker/entrypoint.sh
+chmod +x *.sh
 export DOCKER_BUILDKIT=1
 docker build . -f docker/Dockerfile  -t registry.cn-hangzhou.aliyuncs.com/tuon-pub/nginx-ui:latest
