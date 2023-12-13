3b84c21f0f33e9a3a1773d3844c87728001475df
tuon <976056042@qq.com>
2023-12-13 15:03:22 +0800

    fix： gob

--- app.go
+++ app.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"encoding/gob"
 	"github.com/astaxie/beego"
 	_ "nginx-ui/server/init"
-	"nginx-ui/server/models"
 )
 
 func main() {
-	gob.Register(models.User{})
 	beego.Run()
 }
