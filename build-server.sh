16e398cf08088e5b8be8ba2cf28bf240709f152d
tuon <976056042@qq.com>
2023-12-13 14:47:53 +0800

    fix:打包脚本

--- build-server.sh
+++ build-server.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 
-mkdir -p ./local
-mkdir -p ./local/data/db
-cp -rf ./server/conf ./local
-cp -rf ./server/static ./local
+mkdir -p local/data/db
+mkdir -p local/static/web
 
-cd ./server
+cp -rf ./conf ./local
+
 export GOODS=linux
 export GOARCH=amd64
-go build server/main.go -o ../local/server
-cd $CURRENT_DIR
-cp -rf ./dist/* ./local/static/web/
+go build -o local/server  server/main.go
+
+cp -rf ./frontend/dist/* ./local/static/web/
 rm -rf ./local/static/web/config.js
 chmod +x ./local/server
 
